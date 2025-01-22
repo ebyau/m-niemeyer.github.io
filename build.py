8a4c74f008eac6b129945ab11c3505f040384c54
Brian Ebiyau <38182764+ebyau@users.noreply.github.com>
2025-01-23 01:24:48 +0200

    Update build.py

--- build.py
+++ build.py
@@ -1,11 +1,11 @@
 from pybtex.database.input import bibtex
 
 def get_personal_data():
-    name = ["Michael", "Niemeyer"]
-    email = "micniemeyer1@gmail.com"
+    name = ["Brian", "Ebiyau"]
+    email = "bebyau@gmail.com"
     twitter = "Mi_Niemeyer"
     github = "m-niemeyer"
-    linkedin = "michael-niemeyer"
+    linkedin = "brian-ebiyau"
     bio_text = f"""
                 <p>
                     I am a senior research scientist at Google working on 3D computer vision and generative modeling.
